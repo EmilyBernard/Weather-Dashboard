bdc57d3c55b07069c61f35503a56195f91540a73
EmilyBernard <bernam2000@hotmail.com>
2023-03-27 18:26:00 -0400

    fixed links in html, add all css hopefully, working on functions in script.js

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,31 @@
-const APIKey = "ad431daf2b902fd2d8a3b4c76d3a4c0f"
+var searchButton = document.querySelector("#submit");
 
-var getCurrentWeather = function (city) {
-    var citySearch = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}`
-    fetch(citySearch)
-    .then((response) => response.json() )
-    .then((data) =>  this.displayCurrentWeather(data));
+var APIKey = "ad431daf2b902fd2d8a3b4c76d3a4c0f"
 
-    console.log('Fetch Response \n-------------');
-    console.log(data);
-}
+function handleFormSubmit(event) {
+    event.preventDefault()
+    var city = userInput.value;
+    if (userInput) {
+      getAllWeather(city)
+    }
+  }
 
-var displayCurrentCity = function (data) {
+function getAllWeather(city) {
+ 
+    var APIKey = "ad431daf2b902fd2d8a3b4c76d3a4c0f"
+      var citySearchUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${APIKey}`
+      fetch(citySearchUrl)
+      .then((response) => response.json() )
+      .then((data) =>  displayCurrentWeather(data));
 
-}
+      console.log('Fetch Response');
+      console.log(data);
+  }
+
+
+
+
+  document.querySelector(".search button").addEventListener('click', function(){
+    getAllWeather.search();
+
+  });
